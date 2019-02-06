deabb8e3b05240bbf68add0ee3d7d369ab6fa8dc
Publikwerker <jason.michael.hoffman@gmail.com>
2019-02-06 13:03:44 -0800

    added users endpoints and router. can add users to database

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  PORT: process.env.PORT || 8080
+  DATABASE_URL: process.env.DATABASE_URL || 
+  global.DATABASE_URL,
+  PORT: process.env.PORT || 8080,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '1d'
 };
--- server.js
+++ server.js
@@ -1,9 +1,18 @@
+'use strict';
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const passport = require('passport');
 const cors = require('cors');
-const { CLIENT_ORIGIN, PORT } = require('./config');
+const { CLIENT_ORIGIN, PORT , DATABASE_URL } = require('./config');
 const {deck} = require('./tarotDeck.js');
+const { router: usersRouter } = require('./users');
+
+mongoose.Promise = global.Promise;
 
 const app = express();
+app.use(morgan('common'));
 
 app.use(
   cors({
@@ -11,9 +20,60 @@ app.use(
   })
 );
 
+app.use('/api/users', usersRouter);
+
 app.get('/api/tarotDeck', (req ,res) => {
   console.log(deck);
   res.json(deck);
 });
 
-app.listen(PORT, () => {console.log(`server is running on port ${PORT}`)});
+//POST /api/users/ to request to register a new user
+
+//POST /api/auth/login to request a JWT
+
+//GET /api/protected
+
+//POST /api/auth/refresh
+
+// app.listen(PORT, () => {console.log(`server is running on port ${PORT}`)});
+
+let server;
+
+function runServer() {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(DATABASE_URL,{useNewUrlParser: true}, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app
+        .listen(PORT, () => {
+          console.log(`Your app is listening on port ${PORT}`);
+          resolve();
+        })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
