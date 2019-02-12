aebc152bb8105cad5e0cf9c1952863b57ec174cf
Publikwerker <jason.michael.hoffman@gmail.com>
2019-02-12 15:37:47 -0800

    fixed bugs in auth/router, enhanced reading schema

--- auth/router.js
+++ auth/router.js
@@ -6,6 +6,7 @@ const jwt = require('jsonwebtoken');
 
 const config = require('../config');
 const router = express.Router();
+const {User} = require('../users/models.js');
 
 const createAuthToken = function(user) {
   return jwt.sign({user}, config.JWT_SECRET, {
@@ -28,6 +29,50 @@ const jwtAuth = passport.authenticate('jwt', {session: false});
 router.post('/refresh', jwtAuth, (req, res) => {
   const authToken = createAuthToken(req.user);
   res.json({authToken});
+});
+
+//PUT new reading to history
+router.put('/:id', jwtAuth, (req, res) => {
+  const thisUser = req.params.id;
+
+  const toUpdate = {};
+  const updateableFields = [ 'date', 'comments', 'spread'];
+
+  updateableFields.forEach(field => {
+    if (field in req.body){
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  console.log(toUpdate);
+  User.findOneAndUpdate({username: thisUser})
+    .then(user => {if (!user.history){
+      console.log("doesn't exist");
+      return User.update({username: thisUser}, {
+        $set: { history: toUpdate}
+    }, {new: true},   (err, user) => {
+      // Handle any possible database errors
+      if (err) return res.status(500).send(err);
+      return res.send(user);
+    })
+  } else {
+    console.log('does exist');
+    return User.update({username: thisUser}, {
+      $push:{history: {toUpdate}},
+    }, {new: true},)
+  }
+  })
+  .then(result => {
+    if (result) {
+      res.json(result);
+    } else {
+      next();
+    }
+  })
+  .catch(err => {
+    next(err);
+  });
+
 });
 
 module.exports={router};
--- users/models.js
+++ users/models.js
@@ -4,17 +4,56 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
+const ReadingSchema = mongoose.Schema({
+  date: {type: String,required: true,},
+  comments: {type: String,},
+  query: {type: String,},
+  spread: [{
+    id: {type: String,},
+    name: {type: String,},
+    desc: {type: String,}, 
+    image: {type: String,},
+    facing: {type: Number,},
+  }],
+  userId: {
+    type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true
+  },
+});
+
+//Add `createdAt` and `updatedAt` fields
+ReadingSchema.set('timestamps', true);
+
+// Transform output during `res.json(data)`, `console.log(data)` etc.
+ReadingSchema.set('toJSON', {
+  virtuals: true,
+  transform: (doc, result) => {
+    delete result._id;
+    delete result.__v;
+  }
+});
+
 const UserSchema = mongoose.Schema({
   username: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
-    required: true
-  }
+    required: true,
+  },
+  history: [ReadingSchema],
 });
+
+// ReadingSchema.methods.serialize = function() {
+//   return {
+//     userId: this.userId || ''
+//   };
+// };
+
+// ReadingSchema.methods.validateUserId = function(userId) {
+//   return bcrype.compare(userId, this.userId);
+// };
 
 UserSchema.methods.serialize = function() {
   return {
@@ -26,10 +65,15 @@ UserSchema.methods.validatePassword = function(password) {
   return bcrypt.compare(password, this.password);
 };
 
+UserSchema.methods.getHistory = function() {
+  return this.history;
+};
+
 UserSchema.statics.hashPassword = function(password){
   return bcrypt.hash(password, 10);
 };
 
 const User = mongoose.model('User', UserSchema);
+const Reading = mongoose.model('Reading', ReadingSchema);
 
-module.exports = {User};
+module.exports ={User, Reading};
