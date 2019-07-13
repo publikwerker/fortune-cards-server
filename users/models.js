085bf5a4403f9d5d3347507b12dd0b0644a39247
Jason Hoffman <jason.michael.hoffman@gmail.com>
2019-07-13 15:59:27 -0700

    fixed bug in models, tests pass

--- users/models.js
+++ users/models.js
@@ -51,12 +51,14 @@ const UserSchema = mongoose.Schema({
     maxlength: 72
   },
   history: [ReadingSchema],
-  email: String,
-  trim: true,
-  unique: true,
-  validate(value) {
-    if (!validator.isEmail(value)) {
-      throw new Error('Email is invalid.');
+  email: {
+    type: String,
+    trim: true,
+    unique: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid.')
+      }
     }
   }
 });
